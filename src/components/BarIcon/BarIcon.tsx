5001f09a223c9ce04e3654652591173e22fc96dd
Aram <aramsuqiasyan20@gmail.com>
2022-09-06 04:12:54 +0400

    add bar

--- src/components/BarIcon/BarIcon.tsx
+++ src/components/BarIcon/BarIcon.tsx
@@ -1,7 +1,104 @@
-import React, {memo} from 'react';
+import React, {memo, useMemo} from 'react';
 
-export const BarIcon = memo(() => {
+import classes from './BarIcon.module.scss'
+import activity_monitor from '../../assets/icons/activity_monitor.png'
+import app_store from '../../assets/icons/app_store.png'
+import books from '../../assets/icons/books.png'
+import calculator from '../../assets/icons/calculator.png'
+import discord from '../../assets/icons/discord.png'
+import facebook from '../../assets/icons/facebook.png'
+import finder from '../../assets/icons/finder.png'
+import github from '../../assets/icons/github.png'
+import google_chrome from '../../assets/icons/google_chrome.png'
+import instagram from '../../assets/icons/instagram.png'
+import launchpad from '../../assets/icons/launchpad.png'
+import notes from '../../assets/icons/notes.png'
+import photos from '../../assets/icons/photos.png'
+import qr_encoder from '../../assets/icons/qr_encoder.png'
+import safari from '../../assets/icons/safari.png'
+import settings from '../../assets/icons/settings.png'
+import spotify from '../../assets/icons/spotify.png'
+import telegram from '../../assets/icons/telegram.png'
+import terminal from '../../assets/icons/terminal.png'
+import trash from '../../assets/icons/trash.png'
+import twitter from '../../assets/icons/twitter.png'
+import whatsapp from '../../assets/icons/whatsapp.png'
+import xcode from '../../assets/icons/xcode.png'
+import youtube from '../../assets/icons/youtube.png'
+
+type IconName =
+    "activity_monitor"
+    | "app_store"
+    | "books"
+    | "calculator"
+    | "discord"
+    | "facebook"
+    | "finder"
+    | "github"
+    | "google_chrome"
+    | "instagram"
+    | "launchpad"
+    | "notes"
+    | "photos"
+    | "qr_encoder"
+    | "safari"
+    | "settings"
+    | "spotify"
+    | "telegram"
+    | "terminal"
+    | "trash"
+    | "twitter"
+    | "whatsapp"
+    | "xcode"
+    | "youtube";
+
+const IconsMap: { [key: string]: any } = {
+    activity_monitor,
+    app_store,
+    books,
+    calculator,
+    discord,
+    facebook,
+    finder,
+    github,
+    google_chrome,
+    instagram,
+    launchpad,
+    notes,
+    photos,
+    qr_encoder,
+    safari,
+    settings,
+    spotify,
+    telegram,
+    terminal,
+    trash,
+    twitter,
+    whatsapp,
+    xcode,
+    youtube
+}
+
+interface Props {
+    iconName: IconName,
+    tooltip?: string;
+    onClick?: () => void
+}
+
+export const BarIcon: React.FC<Props> = memo(({iconName, tooltip, onClick}) => {
+
+    const iconSrc = useMemo(() => IconsMap[iconName], [iconName])
+
     return (
-        <div></div>
+        <div className={classes.barIconContainer} onClick={onClick}>
+            <img src={iconSrc} alt={iconSrc} className={classes.barIcon}/>
+            {!!tooltip && (
+                <span className={classes.tooltip}>
+                    <span className={classes.tooltipArrow}/>
+                    <span className={classes.tooltipInner}>{tooltip}</span>
+                </span>
+            )
+            }
+        </div>
     )
 })
--- src/components/MenuBar/MenuBar.tsx
+++ src/components/MenuBar/MenuBar.tsx
@@ -1,7 +1,37 @@
 import React, {memo} from 'react';
 
+import classes from './MenuBar.module.scss'
+import {BarIcon} from "../BarIcon/BarIcon";
+
 export const MenuBar = memo(() => {
     return (
-        <div></div>
+        <div className={classes.menuBarContainer}>
+            <div className={classes.menuBar}>
+                <BarIcon iconName={'finder'}/>
+                <BarIcon iconName={'activity_monitor'}/>
+                <BarIcon iconName={'app_store'}/>
+                <BarIcon iconName={'books'}/>
+                <BarIcon iconName={'calculator'}/>
+                <BarIcon iconName={'discord'}/>
+                <BarIcon iconName={'facebook'}/>
+                <BarIcon iconName={'github'}/>
+                <BarIcon iconName={'google_chrome'}/>
+                <BarIcon iconName={'instagram'}/>
+                <BarIcon iconName={'notes'}/>
+                <BarIcon iconName={'photos'}/>
+                <BarIcon iconName={'qr_encoder'}/>
+                <BarIcon iconName={'launchpad'} tooltip={'Launchpad'}/>
+                <BarIcon iconName={'safari'}/>
+                <BarIcon iconName={'settings'}/>
+                <BarIcon iconName={'spotify'}/>
+                <BarIcon iconName={'telegram'}/>
+                <BarIcon iconName={'terminal'}/>
+                <BarIcon iconName={'twitter'}/>
+                <BarIcon iconName={'whatsapp'}/>
+                <BarIcon iconName={'xcode'}/>
+                <BarIcon iconName={'youtube'}/>
+                <BarIcon iconName={'trash'}/>
+            </div>
+        </div>
     )
 })
